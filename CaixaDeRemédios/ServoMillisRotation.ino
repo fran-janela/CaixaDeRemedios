fb0e73d752e6ebb36a6059fe979a2b7381974d36
fran-janela <francisco.pinheiro.janela@gmail.com>
2020-09-26 11:09:53 -0300

    Primeiro Commit

---
+++ CaixaDeRemédios/ServoMillisRotation.ino
@@ -0,0 +1,43 @@
+#include <Servo.h>
+
+#define LED 12
+#define SERVO 7
+
+unsigned long currentTime;
+unsigned long previousTime = 0;
+Servo servo;
+
+
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(LED, OUTPUT);
+  servo.attach(SERVO);
+
+  servo.write(0); //Calibrando o Servo na posição 0 graus
+}
+
+
+void loop() {
+  currentTime = millis();
+  if(currentTime - previousTime > 10000){  // para 4 horas: 14,400,000
+    previousTime = currentTime;
+    Serial.print("Dispensando...   ");
+    dispenser();
+    Serial.println("Feito!");
+  }
+}
+
+
+// ========= Dispenser =========
+
+void dispenser() {
+  servo.write(180);
+  digitalWrite(LED, HIGH);
+  delay(2000);
+  servo.write(0);
+  digitalWrite(LED, LOW);
+}
+
+
